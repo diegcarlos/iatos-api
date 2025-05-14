57de1a3cb44dd41033bdbfe85dfd584c47e8cbb8
diegcarlos <diegcarlos@gmail.com>
2025-05-13 20:39:34 -0400

    Adiciona novos tipos de imagem permitidos no filtro do multer
    - Atualiza a lista de tipos de arquivo permitidos no middleware de upload para incluir "image/webp" e "image/jpg", ampliando a compatibilidade com diferentes formatos de imagem.

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -5,7 +5,13 @@ const fileFilter = (
   file: Express.Multer.File,
   cb: multer.FileFilterCallback
 ) => {
-  const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "image/webp",
+    "image/jpg",
+  ];
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
